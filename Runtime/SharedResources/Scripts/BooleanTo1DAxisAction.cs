8aab47833fd964781cff3355d8b361c4ab9a6e02
extendreality <hello@extendreality.io>
2020-03-03 08:50:00 +0000

    feat(structure): create initial prefabs and documentation
    The initial prefabs and relevant documentation for the CombinedActions
    has been added to the repo.

---
+++ Runtime/SharedResources/Scripts/BooleanTo1DAxisAction.cs
@@ -0,0 +1,64 @@
+﻿namespace Tilia.Input.CombinedActions
+{
+    using Malimbe.MemberChangeMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+
+    /// <summary>
+    /// Converts a positive and negative boolean representation into a 1D axis float action.
+    /// </summary>
+    public class BooleanTo1DAxisAction : FloatAction
+    {
+        #region Axis Settings
+        /// <summary>
+        /// The <see cref="BooleanAction"/> that represents the negative direction of the Axis.
+        /// </summary>
+        [Serialized]
+        [field: Header("Axis Settings"), DocumentedByXml]
+        public BooleanAction NegativeInput { get; set; }
+        /// <summary>
+        /// The <see cref="BooleanAction"/> that represents the positive direction of the Axis.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml]
+        public BooleanAction PositiveInput { get; set; }
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Serialized]
+        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
+        public BooleanTo1DAxisActionConfigurator Configuration { get; protected set; }
+        #endregion
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Configuration.SetPositiveInputSource(PositiveInput);
+            Configuration.SetNegativeInputSource(NegativeInput);
+        }
+
+        /// <summary>
+        /// Called after <see cref="PositiveInput"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(PositiveInput))]
+        protected virtual void OnAfterPositiveInputChange()
+        {
+            Configuration.SetPositiveInputSource(PositiveInput);
+        }
+
+        /// <summary>
+        /// Called after <see cref="NegativeInput"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(NegativeInput))]
+        protected virtual void OnAfteNegativeInputChange()
+        {
+            Configuration.SetNegativeInputSource(NegativeInput);
+        }
+    }
+}
---
+++ Runtime/SharedResources/Scripts/BooleanTo1DAxisActionConfigurator.cs
@@ -0,0 +1,58 @@
+﻿namespace Tilia.Input.CombinedActions
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+
+    /// <summary>
+    /// Sets up the BooleanTo1DAxisAction prefab based on the provided user settings.
+    /// </summary>
+    public class BooleanTo1DAxisActionConfigurator : MonoBehaviour
+    {
+        #region Axis Settings
+        /// <summary>
+        /// The <see cref="BooleanAction"/> that represents the negative direction of the Axis.
+        /// </summary>
+        [Serialized]
+        [field: Header("Axis Settings"), DocumentedByXml, Restricted]
+        public BooleanAction NegativeInput { get; set; }
+        /// <summary>
+        /// The <see cref="BooleanAction"/> that represents the positive direction of the Axis.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Restricted]
+        public BooleanAction PositiveInput { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Adds the given <see cref="source"/> to the <see cref="PositiveInput"/> Sources collection.
+        /// </summary>
+        /// <param name="source">The <see cref="BooleanAction"/> to add to the Sources collection.</param>
+        public virtual void SetPositiveInputSource(BooleanAction source)
+        {
+            SetInputSource(source, PositiveInput);
+        }
+
+        /// <summary>
+        /// Adds the given <see cref="source"/> to the <see cref="NegativeInput"/> Sources collection.
+        /// </summary>
+        /// <param name="source">The <see cref="BooleanAction"/> to add to the Sources collection.</param>
+        public virtual void SetNegativeInputSource(BooleanAction source)
+        {
+            SetInputSource(source, NegativeInput);
+        }
+
+        /// <summary>
+        /// Sets the adds the <see cref="source"/> to the Sources collection of the <see cref="target"/> <see cref="BooleanAction"/>.
+        /// </summary>
+        /// <param name="source">The <see cref="BooleanAction"/> to add as a source.</param>
+        /// <param name="target">The <see cref="BooleanAction"/> to have the Sources collection updated.</param>
+        protected virtual void SetInputSource(BooleanAction source, BooleanAction target)
+        {
+            target.ClearSources();
+            target.AddSource(source);
+        }
+    }
+}
