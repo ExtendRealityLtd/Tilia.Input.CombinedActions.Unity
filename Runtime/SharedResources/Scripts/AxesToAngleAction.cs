f1fd5c54dd2a0bb2faca223dadb7e98d133b8e46
extendreality <hello@extendreality.io>
2020-03-07 20:38:12 +0000

    feat(Action): convert axis data to float angle with deadzone limits
    The AxesToAngleAction takes a horizontal and vertical axis and converts
    it to an angle which can be used to determine the direction in which
    a 2d axis is being pushed. The angle output can then be fed into
    something to provide an action such as rotating the object to face
    in a certain direction.

---
+++ Runtime/SharedResources/Scripts/AxesToAngleAction.cs
@@ -0,0 +1,98 @@
+﻿namespace Tilia.Input.CombinedActions
+{
+    using Malimbe.MemberChangeMethod;
+    using Malimbe.MemberClearanceMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Data.Type;
+
+    public class AxesToAngleAction : FloatAction
+    {
+        #region Axis Settings
+        /// <summary>
+        /// The <see cref="FloatAction"/> that represents the Horizontal Axis.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: Header("Axis Settings"), DocumentedByXml]
+        public FloatAction HorizontalAxis { get; set; }
+        /// <summary>
+        /// The <see cref="FloatAction"/> that represents the Vertical Axis.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: DocumentedByXml]
+        public FloatAction VerticalAxis { get; set; }
+        #endregion
+
+        #region Deadzone Settings
+        /// <summary>
+        /// The bounds in which the Horizontal Axis is considered inactive.
+        /// </summary>
+        [Serialized]
+        [field: Header("Deadzone Settings"), DocumentedByXml, MinMaxRange(-1f, 1f)]
+        public FloatRange HorizontalDeadzone { get; set; } = new FloatRange(-0.75f, 0.75f);
+        /// <summary>
+        /// The bounds in which the Vertical Axis is considered inactive.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, MinMaxRange(-1f, 1f)]
+        public FloatRange VerticalDeadzone { get; set; } = new FloatRange(-0.75f, 0.75f);
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Serialized]
+        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
+        public AxesToAngleActionConfigurator Configuration { get; protected set; }
+        #endregion
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Configuration.SetHorizontalInputSource(HorizontalAxis);
+            Configuration.SetVerticalInputSource(VerticalAxis);
+            Configuration.SetHorizontalDeadzone(HorizontalDeadzone);
+            Configuration.SetVerticalDeadzone(VerticalDeadzone);
+        }
+
+        /// <summary>
+        /// Called after <see cref="HorizontalAxis"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(HorizontalAxis))]
+        protected virtual void OnAfterHorizontalAxisChange()
+        {
+            Configuration.SetHorizontalInputSource(HorizontalAxis);
+        }
+
+        /// <summary>
+        /// Called after <see cref="VerticalAxis"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(VerticalAxis))]
+        protected virtual void OnAfterVerticalAxisChange()
+        {
+            Configuration.SetVerticalInputSource(VerticalAxis);
+        }
+
+        /// <summary>
+        /// Called after <see cref="HorizontalDeadzone"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(HorizontalDeadzone))]
+        protected virtual void OnAfterHorizontalDeadzoneChange()
+        {
+            Configuration.SetHorizontalDeadzone(HorizontalDeadzone);
+        }
+
+        /// <summary>
+        /// Called after <see cref="VerticalDeadzone"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(VerticalDeadzone))]
+        protected virtual void OnAfterVerticalDeadzoneChange()
+        {
+            Configuration.SetVerticalDeadzone(VerticalDeadzone);
+        }
+    }
+}
---
+++ Runtime/SharedResources/Scripts/AxesToAngleActionConfigurator.cs
@@ -0,0 +1,100 @@
+﻿namespace Tilia.Input.CombinedActions
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Data.Type;
+    using Zinnia.Data.Type.Transformation.Conversion;
+
+    public class AxesToAngleActionConfigurator : MonoBehaviour
+    {
+        #region Axis Settings
+        /// <summary>
+        /// The <see cref="FloatAction"/> that represents the Horizontal Axis.
+        /// </summary>
+        [Serialized]
+        [field: Header("Axis Settings"), DocumentedByXml, Restricted]
+        public FloatAction HorizontalAxis { get; set; }
+        /// <summary>
+        /// The <see cref="FloatAction"/> that represents the Vertical Axis.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Restricted]
+        public FloatAction VerticalAxis { get; set; }
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The horizontal deadzone controller.
+        /// </summary>
+        [Serialized]
+        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
+        public FloatToBoolean HorizontalDeadZone { get; set; }
+        /// <summary>
+        /// The vertical deadzone controller.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Restricted]
+        public FloatToBoolean VerticalDeadZone { get; set; }
+        #endregion
+
+        /// <summary>
+        /// Adds the given <see cref="source"/> to the <see cref="HorizontalAxis"/> Sources collection.
+        /// </summary>
+        /// <param name="source">The <see cref="FloatAction"/> to add to the Sources collection.</param>
+        public virtual void SetHorizontalInputSource(FloatAction source)
+        {
+            SetInputSource(source, HorizontalAxis);
+        }
+
+        /// <summary>
+        /// Adds the given <see cref="source"/> to the <see cref="VerticalAxis"/> Sources collection.
+        /// </summary>
+        /// <param name="source">The <see cref="FloatAction"/> to add to the Sources collection.</param>
+        public virtual void SetVerticalInputSource(FloatAction source)
+        {
+            SetInputSource(source, VerticalAxis);
+        }
+
+        /// <summary>
+        /// Sets the Horizontal Deadzone.
+        /// </summary>
+        /// <param name="source">The <see cref="FloatRange"/> for the deadzone.</param>
+        public virtual void SetHorizontalDeadzone(FloatRange source)
+        {
+            SetDeadzone(source, HorizontalDeadZone);
+        }
+
+        /// <summary>
+        /// Sets the Vertical Deadzone.
+        /// </summary>
+        /// <param name="source">The <see cref="FloatRange"/> for the deadzone.</param>
+        public virtual void SetVerticalDeadzone(FloatRange source)
+        {
+            SetDeadzone(source, VerticalDeadZone);
+        }
+
+        /// <summary>
+        /// Sets the adds the <see cref="source"/> to the Sources collection of the <see cref="target"/> <see cref="FloatAction"/>.
+        /// </summary>
+        /// <param name="source">The <see cref="FloatAction"/> to add as a source.</param>
+        /// <param name="target">The <see cref="FloatAction"/> to have the Sources collection updated.</param>
+        protected virtual void SetInputSource(FloatAction source, FloatAction target)
+        {
+            target.ClearSources();
+            target.AddSource(source);
+        }
+
+        /// <summary>
+        /// Sets the <see cref="target"/> deadzone based on the given <see cref="source"/>.
+        /// </summary>
+        /// <param name="source">The new deadzone range.</param>
+        /// <param name="target">The range to update.</param>
+        protected virtual void SetDeadzone(FloatRange source, FloatToBoolean target)
+        {
+            target.SetPositiveBounds(source.ToVector2());
+        }
+    }
+}
