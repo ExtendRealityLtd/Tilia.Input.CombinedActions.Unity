eac016782567ad3ee9d021a78d4d2982bce23260
extendreality <hello@extendreality.io>
2020-05-22 09:05:52 +0100

    feat(Action): convert angle range to boolean
    The AngleRangeToBoolean prefab utilizes an AxesToAngleAction to
    convert a axis data to an angle and then to determine if that angle
    is within the angle range and setting a boolean action appropriately.
    
    There are two output boolean actions included with the prefab:
    
    * `DeactivateOutsideOfAngleRange` - will deactivate the boolean when
    the current angle is no longer in the set range.
    * `DeactivateWithinAxisDeadzone` - the action will activate when the
    angle is within the set range but will not deactivate when the angle
    is no longer in the set range and will only deactivate when the axis
    falls back within the deadzone.

---
+++ Runtime/SharedResources/Scripts/AngleRangeToBooleanConfigurator.cs
@@ -0,0 +1,63 @@
+﻿namespace Tilia.Input.CombinedActions
+{
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Data.Type.Transformation.Conversion;
+
+    /// <summary>
+    /// Sets up the AngleRangeToBoolean Prefab based on the provided user settings.
+    /// </summary>
+    public class AngleRangeToBooleanConfigurator : MonoBehaviour
+    {
+        #region Facade Settings
+        /// <summary>
+        /// The public interface facade.
+        /// </summary>
+        [Serialized]
+        [field: Header("Facade Settings"), DocumentedByXml, Restricted]
+        public AngleRangeToBooleanFacade Facade { get; protected set; }
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The <see cref="FloatToBoolean"/> for checking the angle states.
+        /// </summary>
+        [Serialized]
+        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
+        public FloatToBoolean AngleChecker { get; protected set; }
+        /// <summary>
+        /// The <see cref="AxesToAngleAction"/> getting the input angle.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, Restricted]
+        public AxesToAngleAction AngleInput { get; protected set; }
+        #endregion
+
+        /// <summary>
+        /// Configures the <see cref="AngleChecker"/> properties.
+        /// </summary>
+        public virtual void ConfigureAngleChecker()
+        {
+            AngleChecker.SetPositiveBounds(Facade.AngleRange.ToVector2());
+        }
+
+        /// <summary>
+        /// Configures the <see cref="AngleInput"/> properties.
+        /// </summary>
+        public virtual void ConfigureAngleInput()
+        {
+            AngleInput.HorizontalAxis = Facade.HorizontalAxis;
+            AngleInput.VerticalAxis = Facade.VerticalAxis;
+            AngleInput.HorizontalDeadzone = Facade.HorizontalDeadzone;
+            AngleInput.VerticalDeadzone = Facade.VerticalDeadzone;
+        }
+
+        protected virtual void OnEnable()
+        {
+            ConfigureAngleChecker();
+            ConfigureAngleInput();
+        }
+    }
+}
---
+++ Runtime/SharedResources/Scripts/AngleRangeToBooleanFacade.cs
@@ -0,0 +1,110 @@
+﻿namespace Tilia.Input.CombinedActions
+{
+    using Malimbe.MemberChangeMethod;
+    using Malimbe.MemberClearanceMethod;
+    using Malimbe.PropertySerializationAttribute;
+    using Malimbe.XmlDocumentationAttribute;
+    using UnityEngine;
+    using Zinnia.Action;
+    using Zinnia.Data.Attribute;
+    using Zinnia.Data.Type;
+
+    /// <summary>
+    /// The public interface into the AngleRangeToBoolean Prefab.
+    /// </summary>
+    public class AngleRangeToBooleanFacade : MonoBehaviour
+    {
+        #region Angle Settings
+        /// <summary>
+        /// The range of the angle to consider true.
+        /// </summary>
+        [Serialized]
+        [field: Header("Target Settings"), DocumentedByXml, MinMaxRange(-180f, 180f)]
+        public FloatRange AngleRange { get; set; } = new FloatRange(-180f, 180f);
+        #endregion
+
+        #region Axis Settings
+        /// <summary>
+        /// The <see cref="FloatAction"/> that represents the Horizontal Axis.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: Header("Axis Settings"), DocumentedByXml]
+        public FloatAction HorizontalAxis { get; set; }
+        /// <summary>
+        /// The <see cref="FloatAction"/> that represents the Vertical Axis.
+        /// </summary>
+        [Serialized, Cleared]
+        [field: DocumentedByXml]
+        public FloatAction VerticalAxis { get; set; }
+        #endregion
+
+        #region Deadzone Settings
+        /// <summary>
+        /// The bounds in which the Horizontal Axis is considered inactive.
+        /// </summary>
+        [Serialized]
+        [field: Header("Deadzone Settings"), DocumentedByXml, MinMaxRange(-1f, 1f)]
+        public FloatRange HorizontalDeadzone { get; set; } = new FloatRange(-0.75f, 0.75f);
+        /// <summary>
+        /// The bounds in which the Vertical Axis is considered inactive.
+        /// </summary>
+        [Serialized]
+        [field: DocumentedByXml, MinMaxRange(-1f, 1f)]
+        public FloatRange VerticalDeadzone { get; set; } = new FloatRange(-0.75f, 0.75f);
+        #endregion
+
+        #region Reference Settings
+        /// <summary>
+        /// The linked Internal Setup.
+        /// </summary>
+        [Serialized]
+        [field: Header("Reference Settings"), DocumentedByXml, Restricted]
+        public AngleRangeToBooleanConfigurator Configuration { get; protected set; }
+        #endregion
+
+        /// <summary>
+        /// Called after <see cref="AngleRange"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(AngleRange))]
+        protected virtual void OnAfterAngleRangeChange()
+        {
+            Configuration.ConfigureAngleChecker();
+        }
+
+        /// <summary>
+        /// Called after <see cref="HorizontalAxis"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(HorizontalAxis))]
+        protected virtual void OnAfterHorizontalAxisChange()
+        {
+            Configuration.ConfigureAngleInput();
+        }
+
+        /// <summary>
+        /// Called after <see cref="VerticalAxis"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(VerticalAxis))]
+        protected virtual void OnAfterVerticalAxisChange()
+        {
+            Configuration.ConfigureAngleInput();
+        }
+
+        /// <summary>
+        /// Called after <see cref="HorizontalDeadzone"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(HorizontalDeadzone))]
+        protected virtual void OnAfterHorizontalDeadzoneChange()
+        {
+            Configuration.ConfigureAngleInput();
+        }
+
+        /// <summary>
+        /// Called after <see cref="VerticalDeadzone"/> has been changed.
+        /// </summary>
+        [CalledAfterChangeOf(nameof(VerticalDeadzone))]
+        protected virtual void OnAfterVerticalDeadzoneChange()
+        {
+            Configuration.ConfigureAngleInput();
+        }
+    }
+}
