92ffe0d89dd4655be657a5946497c3583259189b
extendreality <hello@extendreality.io>
2021-04-04 14:55:00 +0100

    feat(Utility): add prefab creator
    The latest version of Zinnia has the basis of a prefab creator that can
    be used to enable easy adding of prefabs to a scene without needing to
    drag and drop from directories. Instead a new menu item is added for
    quickly adding prefabs. The guide has been updated to accommodate this
    and the FodyWeavers.xml is now located in the root to serve both the
    Runtime and Editor scripts.

---
+++ Editor/Utility/PrefabCreator.cs
@@ -0,0 +1,55 @@
+namespace Tilia.Input.CombinedActions.Utility
+{
+    using System.IO;
+    using UnityEditor;
+    using Zinnia.Utility;
+
+    public class PrefabCreator : BasePrefabCreator
+    {
+        private const string group = "Tilia/";
+        private const string project = "Input/CombinedActions/";
+        private const string menuItemRoot = topLevelMenuLocation + group + subLevelMenuLocation + project;
+
+        private const string package = "io.extendreality.tilia.input.combinedactions.unity";
+        private const string baseDirectory = "Runtime";
+        private const string prefabDirectory = "Prefabs";
+        private const string prefabSuffix = ".prefab";
+
+        private const string prefabCombinedActionsAngleRangeToBoolean = "Input.CombinedActions.AngleRangeToBoolean";
+        private const string prefabCombinedActionsAxesToAngle = "Input.CombinedActions.AxesToAngle";
+        private const string prefabCombinedActionsAxesToVector3Action = "Input.CombinedActions.AxesToVector3Action";
+        private const string prefabCombinedActionsBooleanTo1DAxisAction = "Input.CombinedActions.BooleanTo1DAxisAction";
+
+        [MenuItem(menuItemRoot + prefabCombinedActionsAngleRangeToBoolean, false, priority)]
+        private static void AddCombinedActionsAngleRangeToBoolean()
+        {
+            string prefab = prefabCombinedActionsAngleRangeToBoolean + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+
+        [MenuItem(menuItemRoot + prefabCombinedActionsAxesToAngle, false, priority)]
+        private static void AddCombinedActionsAxesToAngle()
+        {
+            string prefab = prefabCombinedActionsAxesToAngle + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+
+        [MenuItem(menuItemRoot + prefabCombinedActionsAxesToVector3Action, false, priority)]
+        private static void AddCombinedActionsAxesToVector3Action()
+        {
+            string prefab = prefabCombinedActionsAxesToVector3Action + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+
+        [MenuItem(menuItemRoot + prefabCombinedActionsBooleanTo1DAxisAction, false, priority)]
+        private static void AddCombinedActionsBooleanTo1DAxisAction()
+        {
+            string prefab = prefabCombinedActionsBooleanTo1DAxisAction + prefabSuffix;
+            string packageLocation = Path.Combine(packageRoot, package, baseDirectory, prefabDirectory, prefab);
+            CreatePrefab(packageLocation);
+        }
+    }
+}
